ac3a2f38544dd2d08f2868344a854564849232c9
Tigran Bantikyan <tigranbantikyan@gmail.com>
2024-06-16 16:26:40 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <NavBar />
+
+    <!-- <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/experience">About</RouterLink>
+        <RouterLink to="/projects">About</RouterLink>
+      </nav>
+    </div> -->
+  </header>
+
+  <!-- <RouterView />3 -->
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
+</script>
