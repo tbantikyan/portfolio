07176cbad8b8fecf25ee2317ff51706be7190f5f
Tigran Bantikyan <59093401+tigranb2@users.noreply.github.com>
2025-04-20 21:42:07 -0700

    feat: added WalletCache skills (#3)

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex h-fit w-fit gap-1 rounded-2xl bg-neutral-500 py-0.5 pl-1.5 pr-2 dark:bg-zinc-700 lg:gap-1.5 lg:py-1 lg:pl-2 lg:pr-3"
+    class="flex h-fit w-fit gap-1 rounded-2xl bg-neutral-500/85 py-0.5 pl-1.5 pr-2 dark:bg-zinc-700 lg:gap-1.5 lg:py-1 lg:pl-2 lg:pr-3"
   >
     <img class="h-4 md:h-5" :src="skillData.icon" />
     <p class="text-xs text-zinc-100 md:text-sm">{{ skillData.name }}</p>
