4fecb136c2c3f91a5aa535c2c54382d8a80f4fa7
Tigran Bantikyan <tigranbantikyan@gmail.com>
2025-04-22 21:33:53 -0700

    feat: style preview links

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -2,7 +2,7 @@
   <div
     class="flex h-fit flex-row rounded-md bg-white py-4 drop-shadow-xl dark:bg-neutral-800 md:w-full"
   >
-    <div class="mx-4 flex flex-col sm:mx-6">
+    <div class="mx-4 w-full flex flex-col sm:mx-6">
       <h2 class="text-base font-bold sm:text-lg md:text-xl">
         {{ previewData.title }}
       </h2>
@@ -22,11 +22,14 @@
           </div>
         </div>
       </div>
-      <RouterLink
-        class="mt-4 text-base text-neutral-800 underline dark:text-white md:text-lg"
-        to="/projects"
-        >View all projects</RouterLink
+      <RouterLink 
+      class="mx-auto w-full bg-zinc-100/80 hover:bg-zinc-200/80 dark:bg-zinc-900/50 hover:dark:bg-zinc-900/80 flex flex-col mt-4 items-center px-1 md:py-1.5 rounded-lg"
+      to="/projects"
       >
+      <p class="text-base text-neutral-800/95 hover:text-black dark:text-stone-100 hover:dark:text-white md:text-lg">
+        View all projects
+      </p>
+      </RouterLink>
     </div>
   </div>
 </template>
